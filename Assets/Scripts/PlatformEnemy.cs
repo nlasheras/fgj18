c7483804d83601a1bf72c90b93f57fde3c3e6ee5
ahonenn <nicolasahonen@gmail.com>
2018-01-27 17:11:32 +0200

    platform enemy, player attack

---
+++ Assets/Scripts/PlatformEnemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformEnemy : PlatformController
+{
+    private Transform target;
+ 
+    // Use this for initialization
+    public override void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start(); 
+    }
+
+    void Update()
+    {
+        float sqrRemainingDistance = (transform.position - target.position).sqrMagnitude;
+
+        if (sqrRemainingDistance <= 3.0f)
+        {
+            Debug.Log("Player Got hit by the enemy!!");
+            //Destroy(target.gameObject);
+        }
+    }
+
+}
