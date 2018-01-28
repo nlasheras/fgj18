d83e1dc3fa204b8620271075f32131d13d389da0
Jeppe <jepperi@gmail.com>
2018-01-28 12:22:21 +0200

    Hacked together a quick titlescreen.

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+    private const string startLevel = "lvl1AttackJumpMove";
+
+	// Update is called once per frame
+	void Update () {
+        if ( Input.GetKeyDown ( KeyCode.Space ) )
+        {
+            SceneManager.LoadScene ( startLevel, LoadSceneMode.Single );
+        }
+    }
+}
